a7b595459973b95d530c8cebbf1ffb3888e9872f
dyeresko <denis.yeresko@gmail.com>
2023-05-01 16:31:24 +0300

    Added project 'The Thing Namer 3000'

---
+++ TheThingNamer3000/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What kind of thing we talking about?");
+// Thing
+string a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+// Type of thing
+string b = Console.ReadLine();
+/* 
+Cool name
+
+
+I can write anything
+in 
+it
+
+*/
+string c = "Doom";
+// The numberr of thing
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
