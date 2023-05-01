c31230b952caf301ab41055119e7e8614cd2f702
dyeresko <denis.yeresko@gmail.com>
2023-05-01 17:14:56 +0300

    Added project 'The Dominion Of Kings'

---
+++ TheDominionOfKings/UI/Program.cs
@@ -0,0 +1,13 @@
+﻿int usersTotalScore = 0;
+
+Console.Write("Enter how many provinces do you have ");
+int amountOfProvinces = int.Parse(Console.ReadLine());
+usersTotalScore += amountOfProvinces * 6;
+Console.Write("Enter how many duchies do you have ");
+int amountOfDuchies = int.Parse(Console.ReadLine());
+usersTotalScore += amountOfDuchies * 3;
+Console.Write("Enter how many estates do you have? ");
+int amountOfEstates = int.Parse(Console.ReadLine());
+usersTotalScore += amountOfEstates;
+
+Console.WriteLine("Your score is " + usersTotalScore);
